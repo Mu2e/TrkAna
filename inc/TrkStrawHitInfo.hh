794ce8978d564b0bfd2b1e2d2e249a123038cd2c
David Nathan Brown <dave_brown@lbl.gov>
2022-08-25 12:05:40 -0500

    Follow changes to TrkStrawHitSeed

--- inc/TrkStrawHitInfo.hh
+++ inc/TrkStrawHitInfo.hh
@@ -17,13 +17,13 @@ namespace mu2e
     Float_t htime;   // raw hit time
     Float_t wdist;       // raw hit U position
     Float_t werr;    // raw hit U position error estimate
-    Float_t totdrift; // TOT expressed as drift time
+    Float_t tottdrift; // TOT expressed as drift time
     Float_t ptoca, stoca;    // reference particle (sensor) time of closest approach (TOCA)
-    Float_t wdoca, wdocavar;   // reference (biased) DOCA from the track to the wire, signed by the angular momentum WRT the wire and the measurement end (and variance)
-    Float_t wdt, wtocavar;   // reference (biased) time difference (and variance) at POCA
-    Float_t doca, docavar; // unbiaed DOCA (and variance)
-    Float_t dt, tocavar;   // fit (unbiased) dt and variance
-    Float_t upos; // POCA position along the straw WRT the straw middle
+    Float_t rdoca, rdocavar;   // reference (biased) DOCA from the track to the wire, signed by the angular momentum WRT the wire and the measurement end (and variance)
+    Float_t rdt, rtocavar;   // reference (biased) time difference (and variance) at POCA
+    Float_t udoca, udocavar; // unbiaed DOCA (and variance)
+    Float_t udt, utocavar;   // unbiased dt and variance
+    Float_t rupos, uupos; // POCA position along the straw WRT the straw middle
     Float_t rdrift,rerr,dvel,lang; // drift information
     Float_t utresid, utresidmvar, utresidpvar; // unbiased time residual and associated measurement and parameter variances
     Float_t udresid, udresidmvar, udresidpvar; // unbiased distance residual and associated measurement and parameter variances
@@ -35,11 +35,11 @@ namespace mu2e
     bool dhit, dactive;
 
     TrkStrawHitInfo() : plane(-1), panel(-1), layer(-1), straw(-1), state(-10),driftend(-1),
-    edep(0), htime(0), wdist(0), werr(0),totdrift(0),
+    edep(0), htime(0), wdist(0), werr(0),tottdrift(0),
     ptoca(0), stoca(0),
-    wdoca(0), wdocavar(0), wdt(0), wtocavar(0),
-    doca(0.0), docavar(0), dt(0), tocavar(0),
-    upos(0),rdrift(0), rerr(0), dvel(0), lang(0),
+    rdoca(0), rdocavar(0), rdt(0), rtocavar(0),
+    udoca(0.0), udocavar(0), udt(0), utocavar(0),
+    rupos(0),uupos(0),rdrift(0), rerr(0), dvel(0), lang(0),
     utresid(0), utresidmvar(0), utresidpvar(0),
     udresid(0), udresidmvar(0), udresidpvar(0),
     rtresid(0), rtresidmvar(0), rtresidpvar(0),
