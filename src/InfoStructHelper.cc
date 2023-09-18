99d519df3c11307adeb7b9af48aba4877cbaec44
Andrew Edmonds <awje@bu.edu>
2023-09-18 08:27:06 -0500

    Fix nightly: change in CRV accessor

--- src/InfoStructHelper.cc
+++ src/InfoStructHelper.cc
@@ -422,9 +422,9 @@ namespace mu2e {
 
   void InfoStructHelper::fillCrvHitInfo(art::Ptr<CrvCoincidenceCluster> const& crvCoinc, CrvHitInfoReco& crvHitInfo) {
     crvHitInfo._crvSectorType = crvCoinc->GetCrvSectorType();
-    crvHitInfo._x = crvCoinc->GetAvgCounterPos().x();
-    crvHitInfo._y = crvCoinc->GetAvgCounterPos().y();
-    crvHitInfo._z = crvCoinc->GetAvgCounterPos().z();
+    crvHitInfo._x = crvCoinc->GetAvgHitPos().x();
+    crvHitInfo._y = crvCoinc->GetAvgHitPos().y();
+    crvHitInfo._z = crvCoinc->GetAvgHitPos().z();
     crvHitInfo._timeWindowStart = crvCoinc->GetStartTime();
     crvHitInfo._timeWindowEnd = crvCoinc->GetEndTime();
     crvHitInfo._PEs = crvCoinc->GetPEs();
