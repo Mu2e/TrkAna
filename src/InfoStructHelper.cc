c536f4dcadf745af50f0b64c28376715abc801e0
David Nathan Brown <dave_brown@lbl.gov>
2024-01-12 13:05:20 -0600

    Update count structs: global hitcount still not filled

--- src/InfoStructHelper.cc
+++ src/InfoStructHelper.cc
@@ -18,22 +18,18 @@ namespace mu2e {
       if(shf.hasAllProperties(StrawHitFlag::radsel))++hitcount.nrsel;
       if(shf.hasAllProperties(StrawHitFlag::timesel))++hitcount.ntsel;
       if(shf.hasAllProperties(StrawHitFlag::bkg))++hitcount.nbkg;
-      if(shf.hasAllProperties(StrawHitFlag::trksel))++hitcount.ntpk;
     }
   }
 
   void InfoStructHelper::fillHitCount(RecoCount const& nrec, HitCount& hitcount) {
-    hitcount.nsd = nrec._nstrawdigi;
-    hitcount.ncd = nrec._ncalodigi;
-    hitcount.ncrvd = nrec._ncrvdigi;
     hitcount.nesel = nrec._nshfesel;
     hitcount.nrsel = nrec._nshfrsel;
     hitcount.ntsel = nrec._nshftsel;
     hitcount.nbkg = nrec._nshfbkg;
-    hitcount.ntpk = nrec._nshftpk;
   }
 
   void InfoStructHelper::fillTrkInfo(const KalSeed& kseed,TrkInfo& trkinfo) {
+    trkinfo.reset();
     if(kseed.status().hasAllProperties(TrkFitFlag::kalmanConverged))
       trkinfo.status = 1;
     else if(kseed.status().hasAllProperties(TrkFitFlag::kalmanOK))
@@ -183,10 +179,12 @@ namespace mu2e {
  }
 
   void InfoStructHelper::fillTrkInfoHits(const KalSeed& kseed, TrkInfo& trkinfo) {
-    trkinfo.nhits = trkinfo.nactive = trkinfo.ndouble = trkinfo.ndactive = trkinfo.nplanes = trkinfo.planespan = trkinfo.nnullambig = 0;
     static StrawHitFlag active(StrawHitFlag::active);
     std::set<unsigned> planes;
     uint16_t minplane(0), maxplane(0);
+    static StrawHitFlag allsel("EnergySelection:TimeSelection:RadiusSelection");
+    static StrawHitFlag allrej("Background:Dead:Noisy");
+
     for (auto ihit = kseed.hits().begin(); ihit != kseed.hits().end(); ++ihit) {
       ++trkinfo.nhits;
       if (ihit->strawHitState() > WireHitState::inactive){
@@ -197,6 +195,13 @@ namespace mu2e {
         if (ihit->strawHitState()==WireHitState::null) {
           ++trkinfo.nnullambig;
         }
+        // count active hits by flag state
+        if(ihit->flag().hasAllProperties(StrawHitFlag::energysel))trkinfo.nesel++;
+        if(ihit->flag().hasAllProperties(StrawHitFlag::radsel))trkinfo.nrsel++;
+        if(ihit->flag().hasAllProperties(StrawHitFlag::timesel))trkinfo.ntsel++;
+        if(ihit->flag().hasAllProperties(StrawHitFlag::bkg))trkinfo.nbkg++;
+        if(ihit->flag().hasAllProperties(allsel) && (!ihit->flag().hasAnyProperty(allrej)))trkinfo.nsel++;
+        // these variables are obsolete, use stereo hits instead TODO
         auto jhit = ihit; jhit++;
         if(jhit != kseed.hits().end() && ihit->strawId().uniquePanel() == jhit->strawId().uniquePanel()){
           ++trkinfo.ndouble;
