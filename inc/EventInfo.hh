766373c9f2dd66483780c12e172adbedf50b375e
Andrew Edmonds <aedmonds@york.cuny.edu>
2024-07-29 16:47:19 -0500

    Update EventInfo struct so that there is a comment per variable

--- inc/EventInfo.hh
+++ inc/EventInfo.hh
@@ -8,12 +8,12 @@
 namespace mu2e
 {
   struct EventInfo {
-    int event = 0;
-    int run = 0;
-    int subrun = 0; // run/event identification
+    int event = 0; // event number
+    int run = 0; // run number
+    int subrun = 0; // subrun number
     int nprotons = 0; // estimated # of protons on target for this microbunch
-    float pbtime = 0.0;
-    float  pbterr = 0.0; // estimated proton bunch time (and error)
+    float pbtime = 0.0; // estimated proton bunch time
+    float  pbterr = 0.0; // estimated proton bunch time uncertainty
     void reset() {*this = EventInfo(); }
   };
 }
