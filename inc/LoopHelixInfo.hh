4525f5541490ce74f246a22ebafe47ee7c360f0b
Andrew Edmonds <AndrewEdmonds11@users.noreply.github.com>
2024-09-23 09:34:01 -0400

    Revert "Updating InfoStructs for ntuplehelper"

--- inc/LoopHelixInfo.hh
+++ inc/LoopHelixInfo.hh
@@ -1,5 +1,5 @@
 //
-// LoopHelixInfo: array of array branch for LoopHelix track parameterization (used for looping tracks). Example structure: [ [trk1_surfA, trk1_surfB, ...], [trk2_surf_A, trk2_surfC], ... [ trkN_surfB, trkN_surfC] ]
+// LoopHelixInfo: branch for LoopHelix track parameterization used for looping tracks
 // All momenta are in units of MeV/c, time in nsec WRT when the proton bunch pulse peak hits the production target,
 // positions are in mm WRT the center of the tracker.
 // Dave Brown (LBNL)
@@ -12,22 +12,12 @@ namespace mu2e
   struct LoopHelixInfo {
     //  These members are deprecated
     // in favor of explicit extrapolation to the OPA
-    float maxr = 0; // max radius (assuming geometric extrapolation) [mm] DEPRECATED
-    float d0 = 0; // d0 (unsigned!) [mm]  DEPRECATED
+    float maxr = 0; // max radius (assuming geometric extrapolation) DEPRECATED
+    float d0 = 0; // d0 (unsigned!)  DEPRECATED
     float tanDip = 0; // tanDip  DEPRECATED
     // loop helix parameters
-    float rad = 0; // transverse radius of this LoopHelix segment [mm], see Figure 1 of DocDB-46577
-    float lam = 0; // longitudiinal wavelength of this LoopHelix segment [mm], see Figure 1 of DocDB-46577
-    float cx = 0; // x-position of the center of this LoopHelix segment [mm], see Figure 1 of DocDB-46577
-    float cy = 0; // y-position of the center of this LoopHelix segment [mm], see Figure 1 of DocDB-46577
-    float phi0 = 0; // phi0 parameter of this LoopHelix sgement, see Figure 1 of DocDB-46577
-    float t0 = 0; // time of this LoopHelix segment [ns]
-    float raderr = 0; // uncertainty on transverse radius [mm]
-    float lamerr = 0; // uncertainty on longitudinal wavelength [mm]
-    float cxerr = 0; // uncertainty on x-position of the center [mm]
-    float cyerr = 0; // uncertainty on y-position of the center [mm]
-    float phi0err = 0; // uncertainty on phi0 parameter
-    float t0err = 0; // uncertainty on time parameter [ns]
+    float rad=0,lam=0,cx=0,cy=0,phi0=0,t0=0;
+    float raderr=0,lamerr=0,cxerr=0,cyerr=0,phi0err=0,t0err=0;
     void reset() { *this = LoopHelixInfo(); }
   };
 }
--- inc/TrkFitInfo.hh
+++ inc/TrkFitInfo.hh
@@ -1,5 +1,5 @@
 //
-// TrkFitInfo: an array of array branch that contains track fit results at particular Surfaces. Example structure: [ [trk1_surfA, trk1_surfB, ...], [trk2_surf_A, trk2_surfC], ... [ trkN_surfB, trkN_surfC] ]
+// TrkFitInfo: generic information about the track fit at a particular Surface
 // All momenta are in units of MeV/c, time in nsec WRT when the proton bunch pulse peak hits the production target,
 // positions are in mm WRT the center of the tracker.
 // Dave Brown (LBNL)
@@ -11,15 +11,15 @@
 namespace mu2e
 {
   struct TrkFitInfo {
-    XYZVectorF mom = XYZVectorF(); // momentum of the fit at this Surface
-    XYZVectorF pos = XYZVectorF(); // position of the fit at this Surface
-    double time = 0.0; // time of the fit at this Surface
+    XYZVectorF mom;
+    XYZVectorF pos;
+    double time;
     float momerr = -1000;  // projected error on the scalar momentum
-    bool inbounds = false; // was the intersection within the (literal) surface bounds?
-    bool gap = false; // was the intersection in a piecewise trajectory gap?
-    bool early = false; // is this the earliest intersection for this track?
-    bool late = false; // is this the latest intersection for this track?
-    int sid = SurfaceIdEnum::unknown; // SurfaceId of the intersected surface, see Offline/KinKalGeom/inc/SurfaceId.hh for definitions
+    bool inbounds = false; // was the intersection within the (literal) surface bounds
+    bool gap = false; // was the intersection in a piecewise trajectory gap
+    bool early = false; // is this the earliest intersection for this track
+    bool late = false; // is this the latest intersection for this track
+    int sid = SurfaceIdEnum::unknown; // SurfaceId of the intersected surface, see Offline/KinKalGeom/inc/SurfaceId.hh
     int sindex = 0; // index to the intersected surface (for multi-surface elements like StoppingTarget)
     void reset() { *this = TrkFitInfo(); }
   };
