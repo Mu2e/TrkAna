34f341ee4bccb94a484fa86a426de42aee8ff69a
Andrew Edmonds <aedmonds@york.cuny.edu>
2024-09-23 19:41:25 -0500

    Update validation script for new "trk" branch. Don't rename histograms themselves yet...

--- validation/create_val_file.C
+++ validation/create_val_file.C
@@ -17,26 +17,26 @@ void create_val_file(std::string filename, std::string outfilename) {
   ntuple->Draw("evtinfomc.nprotons>>h_evtinfomc_nprotons", "", "");
   ntuple->Draw("evtinfomc.pbtime>>h_evtinfomc_pbtime", "", "");
 
-  // demfit histograms
-  ntuple->Draw("demfit.mom.R()>>h_demfit_mom_all", "", "goff");
-  ntuple->Draw("demfit.mom.R()>>h_demfit_mom_ent", "demfit.sid==0", "goff");
-  ntuple->Draw("demfit.mom.R()>>h_demfit_mom_mid", "demfit.sid==1", "goff");
-  ntuple->Draw("demfit.mom.R()>>h_demfit_mom_xit", "demfit.sid==2", "goff");
-  ntuple->Draw("demfit.mom.R()>>h_demfit_mom_ent_t0cut", "demfit.sid==0 && demlh.t0>=700", "goff");
-  ntuple->Draw("demfit.mom.R()>>h_demfit_mom_mid_t0cut", "demfit.sid==1 && demlh.t0>=700", "goff");
-  ntuple->Draw("demfit.mom.R()>>h_demfit_mom_xit_t0cut", "demfit.sid==2 && demlh.t0>=700", "goff");
+  // trkfit histograms
+  ntuple->Draw("trkfit.mom.R()>>h_demfit_mom_all", "", "goff");
+  ntuple->Draw("trkfit.mom.R()>>h_demfit_mom_ent", "trkfit.sid==0", "goff");
+  ntuple->Draw("trkfit.mom.R()>>h_demfit_mom_mid", "trkfit.sid==1", "goff");
+  ntuple->Draw("trkfit.mom.R()>>h_demfit_mom_xit", "trkfit.sid==2", "goff");
+  ntuple->Draw("trkfit.mom.R()>>h_demfit_mom_ent_t0cut", "trkfit.sid==0 && trklh.t0>=700", "goff");
+  ntuple->Draw("trkfit.mom.R()>>h_demfit_mom_mid_t0cut", "trkfit.sid==1 && trklh.t0>=700", "goff");
+  ntuple->Draw("trkfit.mom.R()>>h_demfit_mom_xit_t0cut", "trkfit.sid==2 && trklh.t0>=700", "goff");
 
   // resolution histograms
-  ntuple->Draw("(demfit[][demmcvd.iinter].mom.R() - demmcvd[].mom.R())>>h_demfit_momres_all", "", "goff");
-  ntuple->Draw("(demfit[][demmcvd.iinter].mom.R() - demmcvd[].mom.R())>>h_demfit_momres_ent", "demmcvd[].sid==0", "goff");
-  ntuple->Draw("(demfit[][demmcvd.iinter].mom.R() - demmcvd[].mom.R())>>h_demfit_momres_mid", "demmcvd[].sid==1", "goff");
-  ntuple->Draw("(demfit[][demmcvd.iinter].mom.R() - demmcvd[].mom.R())>>h_demfit_momres_xit", "demmcvd[].sid==2", "goff");
+  ntuple->Draw("(trkfit[][trkmcvd.iinter].mom.R() - trkmcvd[].mom.R())>>h_demfit_momres_all", "", "goff");
+  ntuple->Draw("(trkfit[][trkmcvd.iinter].mom.R() - trkmcvd[].mom.R())>>h_demfit_momres_ent", "trkmcvd[].sid==0", "goff");
+  ntuple->Draw("(trkfit[][trkmcvd.iinter].mom.R() - trkmcvd[].mom.R())>>h_demfit_momres_mid", "trkmcvd[].sid==1", "goff");
+  ntuple->Draw("(trkfit[][trkmcvd.iinter].mom.R() - trkmcvd[].mom.R())>>h_demfit_momres_xit", "trkmcvd[].sid==2", "goff");
 
   // trkcalohit histograms
-  ntuple->Draw("demtch.ctime>>h_demtch_ctime_all", "", "goff");
-  ntuple->Draw("demtch.ctime>>h_demtch_ctime_active", "demtch.active==1", "goff");
-  ntuple->Draw("demtch.ctime>>h_demtch_ctime_inactive", "demtch.active==0", "goff");
-  ntuple->Draw("demtch.ctime-demlh.t0>>h_demtch_demfit_dt_ent", "demfit.sid==0 && demtch.active==1", "goff");
+  ntuple->Draw("trktch.ctime>>h_demtch_ctime_all", "", "goff");
+  ntuple->Draw("trktch.ctime>>h_demtch_ctime_active", "trktch.active==1", "goff");
+  ntuple->Draw("trktch.ctime>>h_demtch_ctime_inactive", "trktch.active==0", "goff");
+  ntuple->Draw("trktch.ctime-trklh.t0>>h_demtch_demfit_dt_ent", "trkfit.sid==0 && trktch.active==1", "goff");
 
   // crv histograms
   ntuple->Draw("crvsummary.totalPEs>>h_crvsummary_totalPEs_all", "", "goff");
@@ -48,25 +48,25 @@ void create_val_file(std::string filename, std::string outfilename) {
   ntuple->Draw("crvcoincsmc.primary.fCoordinates.fZ>>h_crvhitmc_primary_z", "", "goff");
   ntuple->Draw("crvcoincsmc.depositedEnergy>>h_crvhitmc_depostedEnergy", "", "goff");
 
-  // demmcsim histograms
-  ntuple->Draw("demmcsim.pos.x()>>h_demmcsim_pos_x_all", "", "goff");
-  ntuple->Draw("demmcsim.pos.y()>>h_demmcsim_pos_y_all", "", "goff");
-  ntuple->Draw("demmcsim.pos.z()>>h_demmcsim_pos_z_all", "", "goff");
-  ntuple->Draw("demmcsim.pos.x()>>h_demmcsim_pos_x_evtprimary", "demmcsim.prirel._rel==0 && demmcsim.prirel._rem==0", "goff");
-  ntuple->Draw("demmcsim.pos.y()>>h_demmcsim_pos_y_evtprimary", "demmcsim.prirel._rel==0 && demmcsim.prirel._rem==0", "goff");
-  ntuple->Draw("demmcsim.pos.z()>>h_demmcsim_pos_z_evtprimary", "demmcsim.prirel._rel==0 && demmcsim.prirel._rem==0", "goff");
-  ntuple->Draw("demmcsim.pos.x()>>h_demmcsim_pos_x_trkprimary", "demmcsim.trkrel._rel==0 && demmcsim.trkrel._rem==0", "goff");
-  ntuple->Draw("demmcsim.pos.y()>>h_demmcsim_pos_y_trkprimary", "demmcsim.trkrel._rel==0 && demmcsim.trkrel._rem==0", "goff");
-  ntuple->Draw("demmcsim.pos.z()>>h_demmcsim_pos_z_trkprimary", "demmcsim.trkrel._rel==0 && demmcsim.trkrel._rem==0", "goff");
-  ntuple->Draw("demmcsim.pos.x()>>h_demmcsim_pos_x_trkparent", "demmcsim.trkrel._rel==2 && demmcsim.trkrel._rem==1", "goff");
-  ntuple->Draw("demmcsim.pos.y()>>h_demmcsim_pos_y_trkparent", "demmcsim.trkrel._rel==2 && demmcsim.trkrel._rem==1", "goff");
-  ntuple->Draw("demmcsim.pos.z()>>h_demmcsim_pos_z_trkparent", "demmcsim.trkrel._rel==2 && demmcsim.trkrel._rem==1", "goff");
-  ntuple->Draw("demmcsim.pos.x()>>h_demmcsim_pos_x_trkgparent", "demmcsim.trkrel._rel==5 && demmcsim.trkrel._rem==2", "goff");
-  ntuple->Draw("demmcsim.pos.y()>>h_demmcsim_pos_y_trkgparent", "demmcsim.trkrel._rel==5 && demmcsim.trkrel._rem==2", "goff");
-  ntuple->Draw("demmcsim.pos.z()>>h_demmcsim_pos_z_trkgparent", "demmcsim.trkrel._rel==5 && demmcsim.trkrel._rem==2", "goff");
-  ntuple->Draw("demmcsim.pos.x()>>h_demmcsim_pos_x_trkggparent", "demmcsim.trkrel._rel==5 && demmcsim.trkrel._rem==3", "goff");
-  ntuple->Draw("demmcsim.pos.y()>>h_demmcsim_pos_y_trkggparent", "demmcsim.trkrel._rel==5 && demmcsim.trkrel._rem==3", "goff");
-  ntuple->Draw("demmcsim.pos.z()>>h_demmcsim_pos_z_trkggparent", "demmcsim.trkrel._rel==5 && demmcsim.trkrel._rem==3", "goff");
+  // trkmcsim histograms
+  ntuple->Draw("trkmcsim.pos.x()>>h_demmcsim_pos_x_all", "", "goff");
+  ntuple->Draw("trkmcsim.pos.y()>>h_demmcsim_pos_y_all", "", "goff");
+  ntuple->Draw("trkmcsim.pos.z()>>h_demmcsim_pos_z_all", "", "goff");
+  ntuple->Draw("trkmcsim.pos.x()>>h_demmcsim_pos_x_evtprimary", "trkmcsim.prirel._rel==0 && trkmcsim.prirel._rem==0", "goff");
+  ntuple->Draw("trkmcsim.pos.y()>>h_demmcsim_pos_y_evtprimary", "trkmcsim.prirel._rel==0 && trkmcsim.prirel._rem==0", "goff");
+  ntuple->Draw("trkmcsim.pos.z()>>h_demmcsim_pos_z_evtprimary", "trkmcsim.prirel._rel==0 && trkmcsim.prirel._rem==0", "goff");
+  ntuple->Draw("trkmcsim.pos.x()>>h_demmcsim_pos_x_trkprimary", "trkmcsim.trkrel._rel==0 && trkmcsim.trkrel._rem==0", "goff");
+  ntuple->Draw("trkmcsim.pos.y()>>h_demmcsim_pos_y_trkprimary", "trkmcsim.trkrel._rel==0 && trkmcsim.trkrel._rem==0", "goff");
+  ntuple->Draw("trkmcsim.pos.z()>>h_demmcsim_pos_z_trkprimary", "trkmcsim.trkrel._rel==0 && trkmcsim.trkrel._rem==0", "goff");
+  ntuple->Draw("trkmcsim.pos.x()>>h_demmcsim_pos_x_trkparent", "trkmcsim.trkrel._rel==2 && trkmcsim.trkrel._rem==1", "goff");
+  ntuple->Draw("trkmcsim.pos.y()>>h_demmcsim_pos_y_trkparent", "trkmcsim.trkrel._rel==2 && trkmcsim.trkrel._rem==1", "goff");
+  ntuple->Draw("trkmcsim.pos.z()>>h_demmcsim_pos_z_trkparent", "trkmcsim.trkrel._rel==2 && trkmcsim.trkrel._rem==1", "goff");
+  ntuple->Draw("trkmcsim.pos.x()>>h_demmcsim_pos_x_trkgparent", "trkmcsim.trkrel._rel==5 && trkmcsim.trkrel._rem==2", "goff");
+  ntuple->Draw("trkmcsim.pos.y()>>h_demmcsim_pos_y_trkgparent", "trkmcsim.trkrel._rel==5 && trkmcsim.trkrel._rem==2", "goff");
+  ntuple->Draw("trkmcsim.pos.z()>>h_demmcsim_pos_z_trkgparent", "trkmcsim.trkrel._rel==5 && trkmcsim.trkrel._rem==2", "goff");
+  ntuple->Draw("trkmcsim.pos.x()>>h_demmcsim_pos_x_trkggparent", "trkmcsim.trkrel._rel==5 && trkmcsim.trkrel._rem==3", "goff");
+  ntuple->Draw("trkmcsim.pos.y()>>h_demmcsim_pos_y_trkggparent", "trkmcsim.trkrel._rel==5 && trkmcsim.trkrel._rem==3", "goff");
+  ntuple->Draw("trkmcsim.pos.z()>>h_demmcsim_pos_z_trkggparent", "trkmcsim.trkrel._rel==5 && trkmcsim.trkrel._rem==3", "goff");
 
 
   file->Write();
